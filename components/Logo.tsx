f831ce73476a7efac0bf0e3838bc010fbec5f0ef
Prince Pal <palprince3690@gmail.com>
2024-03-03 18:58:49 +0530

    initial commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <Link href={"/"} className='font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer'>formforge</Link>
   )
 }
 
