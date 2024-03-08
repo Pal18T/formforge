171c7a885f48215ba6adeda39e13cda3c809b419
Prince Pal <palprince3690@gmail.com>
2024-03-08 20:45:39 +0530

    display created forms

--- actions/form.ts
+++ actions/form.ts
@@ -62,3 +62,19 @@ const {name, description} = data;
     }
     return form.id;
 }
+
+export async function GetForms(){
+    const user = await currentUser();
+    if(!user){
+        throw new UserNotFoundErr();
+       }
+    
+       return await prisma.form.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+       })
+}
