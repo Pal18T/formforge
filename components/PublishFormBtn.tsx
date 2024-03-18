ff583b800a78a0436573f866df788ac29a93813d
Prince Pal <palprince3690@gmail.com>
2024-03-18 21:30:09 +0530

    Implementing drag-n-drop

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Button } from './ui/button'
+import { MdOutlinePublish } from 'react-icons/md'
 
 function PublishFormBtn() {
   return (
-    <Button>Publish</Button>
+    <Button  className='gap-2 text-white bg-gradient-to-r from-indigo-400 to-cyan-400'>
+      <MdOutlinePublish className='h-6 w-6'/>
+      Publish</Button>
   )
 }
 
