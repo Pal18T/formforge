38f16ab0bc7c6e588f60fffe8e0aa23198419a28
Prince Pal <palprince3690@gmail.com>
2024-03-23 12:07:45 +0530

    grag-n-drop placement

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -10,7 +10,7 @@ import useDesigner from './hooks/useDesigner';
 import { idGenerator } from '@/lib/idGenerator';
 
 function Designer() {
-    const { elements, addElement} = useDesigner();
+    const { elements, addElement } = useDesigner();
     const droppable = useDroppable({
         id: "designer-drop-area",
         data: {
@@ -20,12 +20,12 @@ function Designer() {
 
     console.log("ELEMENTS", elements);
     useDndMonitor({
-        onDragEnd: (event: DragEndEvent) =>{
+        onDragEnd: (event: DragEndEvent) => {
             const { active, over } = event;
-            if(!active || !over) return;
+            if (!active || !over) return;
 
             const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
-            if(isDesignerBtnElement) {
+            if (isDesignerBtnElement) {
                 const type = active.data?.current?.type;
                 const newElement = FormElements[type as ElementsType].construct(
                     idGenerator()
@@ -57,7 +57,7 @@ function Designer() {
                         </div>
                     )}
                     {elements.length > 0 && (
-                        <div className='flex flex-col text-background w-full gap-2 p-4'>
+                        <div className='flex flex-col w-full gap-2 p-4'>
                             {elements.map(element => (
                                 <DesignerElementWrapper key={element.id} element={element} />
                             ))}
@@ -70,10 +70,38 @@ function Designer() {
     )
 }
 
-function DesignerElementWrapper({element}: {element:FormElementInstance}) {
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+    const topHalf = useDroppable({
+        id: element.id + "-top",
+        data: {
+            type: element.type,
+            elementId: element.id,
+            isTopHalfDesignerElement: true,
+        },
+    });
+
+    const bottomHalf = useDroppable({
+        id: element.id + "-bottom",
+        data: {
+            type: element.type,
+            elementId: element.id,
+            isBottomHalfDesignerElement: true,
+        },
+    })
     const DesignerElement = FormElements[element.type].designerComponent;
 
-    return <DesignerElement elementInstance={element}/>
+    return (
+        <div className='relative h-[120px] flex flex-col text-foreground hover:cursor-pointer rounded-md ring-1 ring-accent ring-inset'>
+        <div ref={topHalf.setNodeRef} className={cn('absolute w-full h-1/2 rounded-t-md')}></div>
+        <div ref={bottomHalf.setNodeRef} className={cn('absolute bg-red-500 w-full bottom-0 h-1/2 rounded-b-md',
+        topHalf.isOver && "bg-green-500"
+        )}></div>
+            <div className='flex w-full h-[120px] items.center rounded-md bg-accent/40 px-4 py-2 pointer-events-none'>
+                <DesignerElement elementInstance={element} />
+
+            </div>
+        </div>
+    );
 }
 
 export default Designer
