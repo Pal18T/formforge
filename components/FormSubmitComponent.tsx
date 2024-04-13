06b949263b8efea3d8ab13dbf925b1cdc46b9768
Prince Pal <palprince3690@gmail.com>
2024-04-13 13:45:07 +0530

    started view submittedForm component

---
+++ components/FormSubmitComponent.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import { FormElementInstance } from './FormElements';
+
+function FormSubmitComponent({formUrl, content}: {
+    formUrl: string;
+    content: FormElementInstance[];
+}) {
+  return (
+    <div>FormSubmitComponent</div>
+  )
+}
+
+export default FormSubmitComponent
